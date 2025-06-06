076bca08caf2b8e3d7aef8fba668690ac849d522
yite <coastq22889@icloud.com>
2025-06-06 09:12:14 +0800

    update go version to 1.24

--- install-go.sh
+++ install-go.sh
@@ -3,13 +3,13 @@
 # This script still need to check
 
 # Step 1: Download the Go tar.gz file
-wget https://go.dev/dl/go1.22.0.linux-amd64.tar.gz
+wget https://go.dev/dl/go1.24.0.linux-amd64.tar.gz
 
 # Step 2: Extract the file to /usr/local
-tar -C /usr/local -xzf go1.22.0.linux-amd64.tar.gz
+tar -C /usr/local -xzf go1.24.0.linux-amd64.tar.gz
 
 # Step 3: Remove the tar.gz file
-rm go1.22.0.linux-amd64.tar.gz
+rm go1.24.0.linux-amd64.tar.gz
 
 # Step 4: Add Go bin to PATH in .profile in an idempotent way
 if ! grep -q '/usr/local/go/bin' $HOME/.profile; then
